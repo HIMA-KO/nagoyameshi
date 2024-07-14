1d79969d8c815652ba6204b501ce5e4f0094b9e3
HIMA-KO <duffy.choco1018@gmail.com>
2024-07-14 15:28:36 +0900

    0714新しく実装した内容

--- src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
@@ -1,11 +1,16 @@
-//package com.example.nagoyameshi.repository;
-//
-//import java.util.List;
-//
-//import com.example.nagoyameshi.entity.Category;
-//
-//public interface CategoryRepository {
-//
-//	List<Category> findAll();
-//
-//}
+package com.example.nagoyameshi.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.nagoyameshi.entity.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+	public Page<Category> findAllByOrderByCreatedAtDesc(Pageable pageable);
+	public Page<Category> findByNameLike(String keyword, Pageable pageable);
+
+
+}
